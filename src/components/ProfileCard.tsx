10e14c8bc24b65b932342084582336dc7aa505ed
fauzanashshidiq <muhzeen07@gmail.com>
2025-08-15 05:59:29 +0700

    feat: add components profile card

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,11 @@
+function ProfileCard() {
+  return (
+    <div>
+      <p>Nama: Fauzan</p>
+      <p>Birth Year: 2003</p>
+      <p>Job: Student</p>
+    </div>
+  );
+}
+
+export default ProfileCard;
