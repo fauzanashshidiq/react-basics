d1400bbe1b3e04472e3b54c7d9addaabe12e5761
fauzanashshidiq <muhzeen07@gmail.com>
2025-08-23 21:27:49 +0700

    feat: array map for profile card

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 import Header from "./components/Header";
 import ProfileCard from "./components/ProfileCard";
 
+type Teacher = {
+  name: string;
+  job: string;
+  year: number;
+  id: number;
+};
+
+const teachers: Teacher[] = [
+  {
+    job: "Dosen",
+    name: "Pak Dhika",
+    year: 1995,
+    id: 1,
+  },
+  {
+    name: "Fauzan",
+    job: "Student",
+    year: 2003,
+    id: 2,
+  },
+  {
+    name: "Yaree",
+    job: "Pengangguran",
+    year: 2003,
+    id: 3,
+  },
+];
+
 function App() {
   return (
     <>
       <Header />
       <div style={{ gap: "8px", display: "flex", flexDirection: "row" }}>
-        <ProfileCard name="Fauzan" job="Student" year={2003} />
-        <ProfileCard job="Dosen" year={1990} />
-        <ProfileCard name="Theo" job="Content Creator" year={200} />
-        <ProfileCard name="Yaree" year={2003} />
+        {teachers.map((teachers) => {
+          return (
+            <ProfileCard
+              name={teachers.name}
+              year={teachers.year}
+              job={teachers.job}
+              key={teachers.id}
+            />
+          );
+        })}
       </div>
     </>
   );
