f384dfe8145bfc5df98339822212f4b5756f368c
fauzanashshidiq <muhzeen07@gmail.com>
2025-08-25 21:12:58 +0700

    feat: add variabel count on counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,6 @@
 const Counter = () => {
+  let count = 0;
+
   const handleDecreament = () => {
     alert("decreament");
   };
@@ -9,7 +11,7 @@ const Counter = () => {
   return (
     <div style={{ display: "flex", gap: "16px" }}>
       <button onClick={handleDecreament}>Kurang</button>
-      <p>0</p>
+      <p>{count}</p>
       <button onClick={handleIncreament}>Tambah</button>
     </div>
   );
